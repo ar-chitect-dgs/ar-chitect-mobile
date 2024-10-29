3335e08a31e8937f00d352277c2a113b0d30b4ac
Julia <65892459+andrmdmd@users.noreply.github.com>
2024-10-29 09:00:02 +0100

    [AS-46] Properly handle showing login screens (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
 import "react-native-gesture-handler";
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 import ARScreen from "./screens/ARScreen";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import { ActivityIndicator, View } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const [user, setUser] = useState<any>(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged((user) => {
+      setUser(user);
+      if (initializing) setInitializing(false);
+    });
+    return unsubscribe;
+  }, [initializing]);
+
+  if (initializing) {
+    // Optional loading indicator while checking auth status
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Login" component={LoginScreen} />
-        <Drawer.Screen name="Register" component={RegisterScreen} />
-        <Drawer.Screen name="AR" component={ARScreen} />
+      <Drawer.Navigator initialRouteName={user ? "Home" : "Login"}>
+        {user ? (
+          <>
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="AR" component={ARScreen} />
+          </>
+        ) : (
+          <>
+            <Drawer.Screen name="Login" component={LoginScreen} />
+            <Drawer.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
       </Drawer.Navigator>
     </NavigationContainer>
   );

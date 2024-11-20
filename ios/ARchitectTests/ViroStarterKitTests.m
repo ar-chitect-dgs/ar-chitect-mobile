dfd5a90b0e619bd4598b4f705b21b0f3532abe65
Marcin Slupczynski <mslupczynski@box.com>
2024-11-20 13:46:31 +0100

    Rename XCode project

--- ios/ARchitectTests/ViroStarterKitTests.m
+++ ios/ARchitectTests/ViroStarterKitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ViroStarterKitTests : XCTestCase
+@interface ARchitectTests : XCTestCase
 
 @end
 
-@implementation ViroStarterKitTests
+@implementation ARchitectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

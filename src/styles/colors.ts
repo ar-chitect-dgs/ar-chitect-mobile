fa6e5750c5897b0a711a3817e6c4c5b47aabd253
Julia <65892459+andrmdmd@users.noreply.github.com>
2024-12-09 22:41:28 +0100

    [AS-47] Add Login/Register/Profile Screens formatting (#21)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,8 @@
 export const purple1 = '#9370FF';
+export const purple2 = '#7489FF';
+
+export const pinkAccent = '#C96BFF';
 
 export const headerColor = '#EFF1FF';
+
+export const textColor = '#2C2D76';
